2b2ec1329ab587b472286b63275e0ef78462b88d
DanSenp <danya20033110@gmail.com>
2024-10-30 19:32:25 +0200

    fixed bugs with main component and autorization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,33 @@
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import LandingPage from './Components/LandingPage/LandingPage';
 import Operations from './Components/Operations/Operations';
 import Cards from './Components/Cards/Cards';
 import AuthorizationConfirm from './Components/AuthorizationConfirm/Authorization';
 import Authorization from './Components/Authorization/Authorization';
 import MainHeader from './Components/MainHeader/MainHeader';
-import AboutUs from './Components/AboutUs/AboutUs'
-import Advantages from './Components/Advantages/Advantages'
-import Reviews from './Components/Reviews/Reviews'
+import AboutUs from './Components/AboutUs/AboutUs';
+import Advantages from './Components/Advantages/Advantages';
+import Reviews from './Components/Reviews/Reviews';
+
 function App() {
+  const location = useLocation();
+
   return (
     <>
-    <MainHeader/>
-    <BrowserRouter>
+      {location.pathname !== '/Authorization' && <MainHeader />}
       <Routes>
-        <Route path="/" element={<Advantages />}/>
-          <Route path="/Reviews" element={<Reviews />}/>
-          <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/Reviews" element={<Reviews />} />
+        <Route path="/Advantages" element={<Advantages />} />
         <Route path="/Authorization" element={<Authorization />} />
-        <Route path="/" element={<LandingPage />}/>
-          <Route path="/Operations" element={<Operations />}/>
-          <Route path="/Cards" element={<Cards />} />
-       <Route path="/Authorization" element={<Authorization />} />
-       <Route path="/AuthorizationConfirm" element={<AuthorizationConfirm />} />
+        <Route path="/LandingPage" element={<LandingPage />} />
+        <Route path="/Operations" element={<Operations />} />
+        <Route path="/Cards" element={<Cards />} />
+        <Route path="/AuthorizationConfirm" element={<AuthorizationConfirm />} />
       </Routes>
-  </BrowserRouter>
-  </>
+    </>
   );
 }
 
