450720454cd7c7a602589113f8622f61d9607267
DanSenp <danya20033110@gmail.com>
2024-11-19 09:53:16 +0200

    change header and font-size fix

---
+++ frontend/src/Components/FunctionHeader/FunctionHeader.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import  './FunctionHeaderStyle.css'
+export default function () {
+  return (
+    <>
+     <div class="topnav">
+      <div className="logo">
+      </div>
+      <div className="links">
+      <div className="nav_el">
+  <a  className='navtext' href="/Operations">Гаманець</a>
+  </div>
+  <div className="nav_el">
+  <a className='navtext' href="/Deposit">Перекази</a>
+  </div>
+  <div className="nav_el">
+  <a className='navtext' href="/">Конвертор</a>
+  </div>
+  </div>
+  <div className="nav_el">
+  <a className='navtext' href="/Profile">
+    <div className="autimg"></div>
+  </a>
+  </div>
+  </div>
+  
+    </>
+  )
+}
